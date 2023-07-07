a9476105c5d9de698e49d3719d5d620743906bca
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-07-07 16:28:25 +0530

    initial-commit

---
+++ src/test/java/com/neon/identitiyaccessmanagement/IdentitiyAccessManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.neon.identitiyaccessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentitiyAccessManagementApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
